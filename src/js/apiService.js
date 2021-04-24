cc2aed41f9316e2381c9dc9c0e78918325318410
Volodymyr Viter <mad.tvido@gmail.com>
2021-04-25 00:42:22 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,16 +2,12 @@ export default class ApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
+        this.KEY = '21227182-bce953b8699f3d441be82715f';
+        this.BASE_URL = 'https://pixabay.com/api';
     }
 
     fetchItems() {
-        // console.log(this);
-        const options = {
-            headers: {
-                key: '21227182-bce953b8699f3d441be82715f',
-            },
-        };
-        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=21227182-bce953b8699f3d441be82715f`
+        const url = `${this.BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${this.KEY}`
 
         return fetch(url)
             .then(r => r.json())
@@ -30,6 +26,6 @@ export default class ApiService {
     }
 
     set query(newQuery) {
-         this.searchQuery = newQuery;
+        this.searchQuery = newQuery;
     }
 }
